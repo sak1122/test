678a1d33d8cf73632b6403cf7a67edbdba021421
sak1122 <62175358+sak1122@users.noreply.github.com>
2022-02-14 20:32:25 +0530

    ended

--- git1.java
+++ git1.java
@@ -1,5 +1,4 @@
 public class git1{
-	System.out.println("sakib khatik");
-	System.out.println(saiutqtq);
+	
 	
 }
--- git2.java
+++ git2.java
@@ -1,7 +1,5 @@
 public class git2{
 	public static void main(String[] args) {
-		System.out.println("s");
-		system.out,println("sameeemushir aptel");
-		asssufaga
+		
 	}
 }
